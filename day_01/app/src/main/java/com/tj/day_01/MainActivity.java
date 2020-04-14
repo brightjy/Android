d34cdd73d63c1d709eeb0f29f1a153d598934710
brightjy <barkji0@naver.com>
2020-04-14 10:49:56 +0900

    first test

---
+++ day_01/app/src/main/java/com/tj/day_01/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tj.day_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButtonClicked(View view){
+        finish();
+    }
+}
---
+++ day_02/app/src/main/java/com/tj/day_02/MainActivity.java
@@ -0,0 +1,47 @@
+package com.tj.day_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn1, btn2, btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // btn1을 클릭할 때 할 일
+                Toast.makeText(getApplicationContext(), "새 창 띄워여", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent); // 새 창 띄우기
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+                startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-5091-0108"));
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
